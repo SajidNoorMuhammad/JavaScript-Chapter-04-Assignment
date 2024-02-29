032230a6dc15bcb3dce42acfcf8086ed474a638c
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-02-29 14:21:11 +0500

    Add Work

---
+++ app.js
@@ -0,0 +1,35 @@
+// Question # 01
+// var helloWorld;
+// var fullName;
+// var fName;
+
+// Question # 02
+// 5 legal Variables
+// var age;
+// var gender;
+// var country;
+// var city;
+// var password;
+
+// 5 Illegal Variables
+// var age_;
+// var 1st;
+// var 76trombones;
+// var dollar$;
+// var sign#;
+
+
+// Question # 03
+var heading = "<h1>“Rules for naming JS variables”</h1>";
+document.write (heading);
+
+var num = "numbers, ";
+var sign = "$ ";
+var word = "letter, ";
+var sens = "sensitive";
+var keys = "keywords";
+
+document.write ("Variable names can only contains " + num + sign + "and ______.  For example: $my_1stVariable </br>");
+document.write ("Variables must begin with a " + word + sign + "or_____. For example $name, _name or name </br>"  );
+document.write ("Variable names are case " + sens + "</br>");
+document.write ("Variable names should not be JS " + keys );
